67201086bf40a6adb1d714ab58b4b13a27c91b45
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2024-06-27 16:50:02 +0800

    clean code

--- cmd/root.go
+++ cmd/root.go
@@ -39,5 +39,5 @@ func init() {
 	rootCmd.AddCommand(tempfileCmd)
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- utils/utils.go
+++ utils/utils.go
@@ -97,8 +97,8 @@ func GitAddAndCommit(fileName string, fileDir string) {
 	//}
 
 	// Add the file to the staging area
-	absFilePath := fileDir + string(os.PathSeparator) + fileName
-	_, err = worktree.Add(absFilePath)
+	//absFilePath := fileDir + string(os.PathSeparator) + fileName
+	_, err = worktree.Add(fileName)
 	if err != nil {
 		fmt.Println("> Error adding file:", err)
 		return
@@ -144,4 +144,33 @@ func CompactDispalyMarkdown(info string) string {
 
 	return compactMdStr
 
+}
+
+// CheckArgs should be used to ensure the right command line arguments are
+// passed before executing an example.
+func CheckArgs(arg ...string) {
+	if len(os.Args) < len(arg)+1 {
+		Warning("Usage: %s %s", os.Args[0], strings.Join(arg, " "))
+		os.Exit(1)
+	}
+}
+
+// CheckIfError should be used to natively panics if an error is not nil.
+func CheckIfError(err error) {
+	if err == nil {
+		return
+	}
+
+	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
+	os.Exit(1)
+}
+
+// Info should be used to describe the example commands that are about to run.
+func Info(format string, args ...interface{}) {
+	fmt.Printf("\x1b[34;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
+}
+
+// Warning should be used to display a warning
+func Warning(format string, args ...interface{}) {
+	fmt.Printf("\x1b[36;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
 }
